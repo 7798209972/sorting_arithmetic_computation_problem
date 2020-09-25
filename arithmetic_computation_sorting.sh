9f3a7b856ace059efc931199dcf03072230b9205
7798209972 <patlesachin1@gmail.com>
2020-09-25 09:28:51 +0530

    Taking Three Inputs

--- arithmetic_computation_sorting.sh
+++ arithmetic_computation_sorting.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 
 #Welcome Message
 
@@ -6,3 +6,16 @@ echo "Welcome to Sorting Arithmetic Computaion Problem";
 echo "================================================";
 printf "\n";
 
+#Getting Three Inputs from User
+
+echo "You have to enter three numeric values for Arithmetic Computations"
+printf "\n";
+
+echo "Enter first value : ";
+read a;
+
+echo "Enter second value : ";
+read b;
+
+echo "Enter third value : ";
+read c;
